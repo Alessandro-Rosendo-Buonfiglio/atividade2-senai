40d4a89ffae8039b80855b7de2ea0ae4ee2647ee
Alessandro Rosendo Buonfiglio <73456854+Alessandro-Rosendo-Buonfiglio@users.noreply.github.com>
2022-07-25 10:24:57 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,11 @@
+var quantidade = 14
+
+for(var n = 0; n <= quantidade; n++){
+    if(n == 0){
+        console.log(n+" -Nulo")
+    }else if(n%2 == 0){
+        console.log(n+" -Par")
+    }else{
+        console.log(n+" -Impar")
+    }
+}
